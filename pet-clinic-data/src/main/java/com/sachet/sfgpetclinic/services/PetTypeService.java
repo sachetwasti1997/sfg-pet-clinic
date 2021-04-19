74785d367d65cb1f6f06fa2219ea8a297d799bc5
sachet_wasti <sachet.w@menerva.tech>
2021-04-19 19:21:05 +0530

    [ADD] Created JPARepositories Service for PetType

--- pet-clinic-data/src/main/java/com/sachet/sfgpetclinic/services/PetTypeService.java
+++ pet-clinic-data/src/main/java/com/sachet/sfgpetclinic/services/PetTypeService.java
@@ -1,4 +1,19 @@
 package com.sachet.sfgpetclinic.services;
 
+import com.sachet.sfgpetclinic.model.PetType;
+
+import java.util.Set;
+
 public interface PetTypeService {
+
+    Set<PetType> findAll();
+
+    PetType findById(Long id);
+
+    PetType save(PetType petType);
+
+    void delete(PetType petType);
+
+    void deleteById(Long id);
+
 }
---
+++ pet-clinic-data/src/main/java/com/sachet/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -0,0 +1,48 @@
+package com.sachet.sfgpetclinic.services.springdatajpa;
+
+import com.sachet.sfgpetclinic.model.PetType;
+import com.sachet.sfgpetclinic.repositories.PetTypeRepository;
+import com.sachet.sfgpetclinic.services.PetTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    @Autowired
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return petTypeRepository.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        petTypeRepository.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
