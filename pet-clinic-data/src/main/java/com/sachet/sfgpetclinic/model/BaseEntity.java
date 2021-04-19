150202f1d31472e2da97c36f0965fd94bb72d86b
sachet_wasti <sachet.w@menerva.tech>
2021-04-19 15:49:21 +0530

    [ADD] Created BaseEntity and mapped other entities to it

--- pet-clinic-data/src/main/java/com/sachet/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/sachet/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,19 @@
 package com.sachet.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+/**
+ * Class not required to be mapped to the database, by the keyword it tells JPA that other classes are going to inherit from the class
+ */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
--- pet-clinic-data/src/main/java/com/sachet/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/sachet/sfgpetclinic/model/Vet.java
@@ -1,4 +1,21 @@
 package com.sachet.sfgpetclinic.model;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
+    Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
