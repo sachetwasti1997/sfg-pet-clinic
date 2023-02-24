27fd106078dfba08aa8a39d9534b6ed398f6e396
sachetwasti1997 <sachetwasti61@gmail.com>
2023-02-24 18:26:46 +0530

    Initialise Project

---
+++ src/main/kotlin/com/sachet/sfgpetclinic/SfgPetClinicApplication.kt
@@ -0,0 +1,11 @@
+package com.sachet.sfgpetclinic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SfgPetClinicApplication
+
+fun main(args: Array<String>) {
+    runApplication<SfgPetClinicApplication>(*args)
+}
---
+++ src/test/kotlin/com/sachet/sfgpetclinic/SfgPetClinicApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sachet.sfgpetclinic
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
